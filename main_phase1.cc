c1f8376955cd82ab983eede0d5ffa56d341db4d0
yizenov <yizenov@ucmerced.edu>
2019-02-19 22:30:26 -0800

    Initial upload of additional content for phase2.

---
+++ main_phase1.cc
@@ -0,0 +1,256 @@
+#include <ctime>
+
+#include "Catalog.h"
+
+using namespace std;
+
+// PHASE #1 Runtime evaluation--------------------------------------------
+int main (int argc, char* argv[]) {
+
+  clock_t begin = clock();
+
+  if (argc != 4) {
+      cout << "Usage: main [sqlite_file] [no_tables] [no_atts]" << endl;
+    return -1;
+  }
+
+  string dbFile = argv[1];
+  int tNo = atoi(argv[2]);
+  int aNo = atoi(argv[3]);
+
+  Catalog catalog(dbFile);
+  cout << catalog << endl; cout.flush();
+
+  ////////////////////////////////
+  for (int i = 0; i < tNo; i++) {
+    char tN[20]; sprintf(tN, "T_%d", i);
+    string tName = tN;
+
+    int taNo = i * aNo;
+    vector<string> atts;
+    vector<string> types;
+    for (int j = 0; j < taNo; j++) {
+      char aN[20]; sprintf(aN, "A_%d_%d", i, j);
+      string aName = aN;
+      atts.push_back(aN);
+
+      string aType;
+      int at = j % 3;
+      if (0 == at) aType = "Integer"; //TODO: Integer or INTEGER
+      else if (1 == at) aType = "Float";
+      else if (2 == at) aType = "String";
+      types.push_back(aType);
+    }
+
+    bool ret = catalog.CreateTable(tName, atts, types);
+    if (true == ret) {
+      cout << "CREATE TABLE " << tName << " OK" << endl;
+
+      for (int j = 0; j < taNo; j++) {
+        unsigned int dist = i * 10 + j;
+        string aN = atts[j];
+        catalog.SetNoDistinct(tName, atts[j], dist);
+      }
+
+      unsigned int tuples = i * 1000;
+      catalog.SetNoTuples(tName, tuples);
+
+      string path = tName + ".dat";
+      catalog.SetDataFile(tName, path);
+    }
+    else {
+      cout << "CREATE TABLE " << tName << " FAIL" << endl;
+    }
+  }
+
+  clock_t end = clock();
+  double elapsed_secs1 = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "Runtime (INSERT TABLES+ATTRIBUTES): " << elapsed_secs1 << endl;
+
+  ////////////////////////////////
+  begin = clock();
+  catalog.Save();
+  end = clock();
+  double elapsed_secs2 = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "Runtime (SAVE TABLES): " << elapsed_secs2 << endl;
+
+  begin = clock();
+  cout << catalog << endl; cout.flush();
+  end = clock();
+  double elapsed_secs3 = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "Runtime (PRINT CATALOG): " << elapsed_secs3 << endl;
+
+  ////////////////////////////////
+  begin = clock();
+  vector<string> tables;
+  catalog.GetTables(tables);
+  for (vector<string>::iterator it = tables.begin();
+       it != tables.end(); it++) {
+    cout << *it << endl;
+  }
+  cout << endl;
+
+  end = clock();
+  double elapsed_secs4 = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "Runtime (PRINT TABLES): " << elapsed_secs4 << endl;
+
+  ////////////////////////////////
+  begin = clock();
+  for (int i = 0; i < 1000; i++) {
+    int r = rand() % tNo + 1; //TODO: need to be in range of number of tables
+    char tN[20]; sprintf(tN, "T_%d", r);
+    string tName = tN;
+
+    unsigned int tuples;
+    catalog.GetNoTuples(tName, tuples);
+    cout << tName << " tuples = " << tuples << endl;
+
+    string path;
+    catalog.GetDataFile(tName, path);
+    cout << tName << " path = " << path << endl;
+
+    vector<string> atts;
+    catalog.GetAttributes(tName, atts);
+    for (vector<string>::iterator it = atts.begin();
+         it != atts.end(); it++) {
+      cout << *it << " ";
+    }
+    cout << endl;
+
+    Schema schema;
+    catalog.GetSchema(tName, schema);
+    cout << schema << endl;
+
+    ////////////////////////////////
+    for (int j = 0; j < 10; j++) {
+      int s = rand() % (r * aNo) + 1; //TODO: rand should be between 0 and n-1
+      char aN[20]; sprintf(aN, "A_%d_%d", r, s);
+      string aName = aN;
+
+      unsigned int distinct;
+      catalog.GetNoDistinct(tName, aName, distinct);
+      cout << tName << "." << aName << " distinct = " << distinct << endl;
+    }
+  }
+
+  end = clock();
+  double elapsed_secs5 = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "Runtime (PRINT SCHEMAS): " << elapsed_secs5 << endl;
+
+  ////////////////////////////////
+  begin = clock();
+  for (int i = 0; i < 5; i++) {
+    char tN[20]; sprintf(tN, "T_%d", i);
+    string tName = tN;
+
+    bool ret = catalog.DropTable(tName);
+    if (true == ret) {
+      cout << "DROP TABLE " << tName << " OK" << endl;
+    }
+    else {
+      cout << "DROP TABLE " << tName << " FAIL" << endl;
+    }
+  }
+  end = clock();
+  double elapsed_secs6 = double(end - begin) / CLOCKS_PER_SEC;
+  cout << "Runtime (DROP TABLES): " << elapsed_secs6 << endl;
+
+//  begin = clock();
+//  catalog.Save();
+//  end = clock();
+//  double elapsed_secs7 = double(end - begin) / CLOCKS_PER_SEC;
+//  cout << "Runtime (SAVE TABLES): " << elapsed_secs7 << endl;
+//
+//  begin = clock();
+//  cout << catalog << endl; cout.flush();
+//  end = clock();
+//  double elapsed_secs8 = double(end - begin) / CLOCKS_PER_SEC;
+
+  cout << "\nRuntime (INSERT TABLES+ATTRIBUTES): " << elapsed_secs1 << endl;
+  cout << "Runtime (SAVE TABLES): " << elapsed_secs2 << endl;
+  cout << "Runtime (PRINT CATALOG): " << elapsed_secs3 << endl;
+  cout << "Runtime (PRINT TABLES): " << elapsed_secs4 << endl;
+  cout << "Runtime (PRINT SCHEMAS): " << elapsed_secs5 << endl;
+  cout << "Runtime (DROP TABLES): " << elapsed_secs6 << endl;
+  //cout << "Runtime (SAVE TABLES): " << elapsed_secs7 << endl;
+  //cout << "Runtime (PRINT CATALOG): " << elapsed_secs8 << endl;
+
+  return 0;
+}
+
+// PHASE #1 Original Console--------------------------------------------
+//Catalog connect_db() {
+//  string dbFile = DB_FILE;
+//  Catalog cat_(dbFile);
+//  return cat_;
+//}
+//
+//int main() {
+//
+//  auto cat = connect_db();
+//  if (!cat.GetConnectionStatus()) {
+//    cout << "NO CONNECTION WITH DB" << endl;
+//    return 0;
+//  }
+//
+//  unsigned user_choice;
+//  while (true) {
+//
+//    cout << "********** Main Menu **********" << endl;
+//    cout << "(1): Create a table" << endl;
+//    cout << "(2): Drop a table" << endl;
+//    cout << "(3): Display the catalog content" << endl;
+//    cout << "(4): Save the catalog content" << endl;
+//    cout << "(5): Exit" << endl;
+//    cout << "Your choice: ";
+//    cin >> user_choice;
+//
+//    if (user_choice == 1) {
+//
+//      string table_name;
+//      cout << "Enter table name: ";
+//      cin >> table_name;
+//
+//      vector<string> attributes, types;
+//      string attribute, type;
+//      int attr_nbr;
+//
+//      cout << "Enter number of attributes: ";
+//      cin >> attr_nbr;
+//
+//      cout << "Enter attribute name and type:" << endl;
+//      for (int i = 0; i < attr_nbr; i++) {
+//        cout << i + 1 << ": ";
+//        cin >> attribute >> type;
+//        attributes.push_back(attribute);
+//        types.push_back(type);
+//      }
+//
+//      cat.CreateTable(table_name, attributes, types);
+//
+//    } else if (user_choice == 2) {
+//      string table_name;
+//      cout << "Enter table name: ";
+//      cin >> table_name;
+//      if (cat.DropTable(table_name))
+//        cout << "table is deleted." << endl;
+//      else
+//        cout << "deletion failed." << endl;
+//    } else if (user_choice == 3) {
+//      cout << cat;
+//    } else if (user_choice == 4) {
+//      if (cat.Save())
+//        cout << "CATALOG IS UP TO DATE" << endl;
+//      else
+//        cout << "ERRORS WHILE UPDATING CATALOG" << endl;
+//    } else if (user_choice == 5) {
+//      //      if (!cat.Save())
+//      //        cout << "ERRORS WHILE UPDATING CATALOG" << endl;
+//      break;
+//    } else {
+//      cout << "INVALID CHOICE" << endl;
+//    }
+//  }
+//
+//  return 0;
+//}
--- makefile
+++ makefile
@@ -7,36 +7,72 @@ ifdef linux
 	tag = -n
 endif
 
-main.out:	main.o Catalog.o ComplexSwapify.o InefficientMap.o Keyify.o Schema.o Swapify.o TwoWayList.o
-	$(CC) -o main.out main.o Catalog.o ComplexSwapify.o InefficientMap.o Keyify.o Schema.o Swapify.o TwoWayList.o $(LIBS)
+main.out:	QueryParser.o QueryLexer.o Schema.o Swapify.o TwoWayList.o Record.o File.o DBFile.o Comparison.o Function.o RelOp.o Catalog.o ComplexSwapify.o InefficientMap.o Keyify.o QueryOptimizer.o QueryCompiler.o main.o
+	$(CC) -o main.out main.o QueryParser.o QueryLexer.o Catalog.o ComplexSwapify.o InefficientMap.o Keyify.o Schema.o Record.o File.o DBFile.o Comparison.o Function.o RelOp.o Swapify.o TwoWayList.o QueryOptimizer.o QueryCompiler.o $(LIBS)
 	
 main.o:	main.cc
 	$(CC) -c main.cc
 	
-Catalog.o: Catalog.h Catalog.cc	InefficientMap.cc TwoWayList.cc
+Catalog.o: Catalog.cc InefficientMap.cc TwoWayList.cc
 	$(CC) -c Catalog.cc
 
-ComplexSwapify.o: ComplexSwapify.h ComplexSwapify.cc Schema.h
+ComplexSwapify.o: ComplexSwapify.cc
 	$(CC) -c ComplexSwapify.cc
 
-#EfficientMap.o: EfficientMap.h EfficientMap.cc
+#EfficientMap.o: EfficientMap.cc
 #	$(CC) -c EfficientMap.cc
 
-InefficientMap.o: InefficientMap.h InefficientMap.cc TwoWayList.h Keyify.cc
+InefficientMap.o: InefficientMap.cc Keyify.cc
 	$(CC) -c InefficientMap.cc
 
-Keyify.o: Keyify.h Keyify.cc
+Keyify.o: Keyify.cc
 	$(CC) -c Keyify.cc
 	
-Schema.o: Schema.h Schema.cc Swap.h	Config.h
+Schema.o: Schema.cc
 	$(CC) -c Schema.cc
 
-Swapify.o: Swapify.h Swapify.cc
+Swapify.o: Swapify.cc
 	$(CC) -c Swapify.cc
 
-TwoWayList.o: TwoWayList.h TwoWayList.cc ComplexSwapify.cc Swapify.h
+TwoWayList.o: TwoWayList.cc ComplexSwapify.cc
 	$(CC) -c TwoWayList.cc
+
+Record.o: Schema.cc Record.cc
+	$(CC) -c Record.cc
+
+File.o: Schema.cc Record.cc File.cc
+	$(CC) -c File.cc
+
+DBFile.o: Schema.cc Record.cc File.cc DBFile.cc
+	$(CC) -c DBFile.cc
+
+Comparison.o: Schema.cc Record.cc Comparison.cc
+	$(CC) -c Comparison.cc
+
+Function.o: Schema.cc Record.cc Function.cc
+	$(CC) -c Function.cc
+
+RelOp.o: Schema.cc Record.cc Comparison.cc RelOp.cc
+	$(CC) -c RelOp.cc
+
+QueryOptimizer.o: Schema.cc Record.cc Comparison.cc RelOp.cc QueryOptimizer.cc
+	$(CC) -c QueryOptimizer.cc
+
+QueryCompiler.o: Schema.cc Record.cc Comparison.cc RelOp.cc QueryOptimizer.cc QueryCompiler.cc
+	$(CC) -c QueryCompiler.cc
+
+QueryParser.o: QueryParser.y
+	yacc --defines=QueryParser.h -o QueryParser.c QueryParser.y
+	sed $(tag) QueryParser.c -e "s/  __attribute__ ((__unused__))$$/# ifndef __cplusplus\n  __attribute__ ((__unused__));\n# endif/"
+	g++ -c QueryParser.c
+
+QueryLexer.o: QueryLexer.l
+	lex -o QueryLexer.c QueryLexer.l
+	gcc -c QueryLexer.c
 
 clean: 
 	rm -f *.o
 	rm -f *.out
+	rm -f QueryLexer.c
+	rm -f QueryParser.c
+	rm -f QueryParser.h
